2265baf10ccd191d42d6fb0cf8e5020bcb938f35
chsohn15 <chsohn15@gmail.com>
2020-09-02 13:25:05 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(){
+
+  return tutorials.map(sentence =>{
+    let sentenceArray = sentence.split(" ")
+    let newArray = sentenceArray.map(word => word[0].toUpperCase() + word.slice(1))
+    let finalArray = newArray.join(" ")
+    return finalArray
+  })
+
 }
+
