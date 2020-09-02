1a1e83a49f77a73ecd83b7e4c0f879cea5895952
slee713 <steven.lee1393@gmail.com>
2020-09-02 13:00:41 -0400

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(titleCase)
+}
+
+function titleCase(str){
+  return str.split(' ').map(
+    function(word){
+      return (word.charAt(0).toUpperCase() + word.slice(1))
+    }
+  ).join(" ")
 }
